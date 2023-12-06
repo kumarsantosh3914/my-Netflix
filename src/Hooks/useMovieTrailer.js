5e07a6d418be8de7974df5b8cc30afd54204b564
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-12-06 19:11:42 +0530

    build MainContainer

---
+++ src/Hooks/useMovieTrailer.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useDispatch } from "react-redux";
+
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  // Fetch trailer video and updating the store with trailer video data
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+
+  return <div>useMovieTrailer</div>;
+};
+
+export default useMovieTrailer;
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "./Hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
@@ -8,6 +10,8 @@ const Browse = () => {
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
